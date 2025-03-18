7c2ba2525031414d41cccad1ee87d1b70d0a8037
Sherzod Artikbayev <sherzadartikabyev@gmail.com>
2025-03-18 22:37:36 +0500

    Navbar Component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { About, Hero, Navbar } from './components'
 const App = () => {
 	return (
 		<main className='relative min-h-screen w-screen overflow-x-hidden bg-zinc-600'>
+			<Navbar />
 			<Hero />
 			<About />
 		</main>
---
+++ src/constants/index.js
@@ -0,0 +1,1 @@
+export const navItem = ['Nexus', 'Vault', 'Prologue', 'About', 'Contact']
