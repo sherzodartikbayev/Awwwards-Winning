b783bba7debca1d8abfcc998e9be822c1280b5ee
Sherzod Artikbayev <sherzadartikabyev@gmail.com>
2025-03-18 21:51:02 +0500

    About Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { Hero } from './components'
+import { About, Hero, Navbar } from './components'
 
 const App = () => {
 	return (
-		<main className='relative min-h-screen w-screen overflow-x-hidden'>
+		<main className='relative min-h-screen w-screen overflow-x-hidden bg-zinc-600'>
 			<Hero />
-			<section className='z-0 min-h-screen bg-blue-500' />
+			<About />
 		</main>
 	)
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { default as Button } from './button'
 export { default as Hero } from './hero'
 export { default as VideoPreview } from './video-preview'
+export { default as About } from './about'
+export { default as AnimatedTitle } from './animated-title'
